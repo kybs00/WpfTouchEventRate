e2722dfe2fb833d4e35ea11b4e1fe0676fd10eac
kybs00 <1016724958@qq.com>
2024-10-12 11:06:55 +0800

    添加项目文件。

---
+++ WpfApp13/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System.Diagnostics;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp13
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            TouchDown += MainWindow_TouchDown;
+            TouchMove += MainWindow_TouchMove;
+            TouchUp += MainWindow_TouchUp;
+        }
+        private int _count = 0;
+        private int _startTick;
+
+        private void MainWindow_TouchMove(object sender, System.Windows.Input.TouchEventArgs e)
+        {
+            _count++;
+        }
+
+        private void MainWindow_TouchDown(object sender, System.Windows.Input.TouchEventArgs e)
+        {
+            _startTick = Environment.TickCount;
+        }
+
+        private void MainWindow_TouchUp(object sender, System.Windows.Input.TouchEventArgs e)
+        {
+            Debug.WriteLine($"{_count}个点，帧率：{_count* 1000 / (Environment.TickCount - _startTick)}");
+        }
+    }
+}
