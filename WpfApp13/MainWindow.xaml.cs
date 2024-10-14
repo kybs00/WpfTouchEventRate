08b0dfae999820d7a227be468eeff4b6f90fbfa5
kybs00 <1016724958@qq.com>
2024-10-14 09:45:10 +0800

    修改命名空间

--- WpfApp13/MainWindow.xaml.cs
+++ WpfApp13/MainWindow.xaml.cs
@@ -1,16 +1,7 @@
 ﻿using System.Diagnostics;
-using System.Text;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
-namespace WpfApp13
+namespace WpfTouchEventRate
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
